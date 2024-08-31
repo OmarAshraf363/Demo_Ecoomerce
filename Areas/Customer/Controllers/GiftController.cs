0b16a3731716745e2dffc09af6d10f192bdd2785
Omar Ashraf <oa38150@gmail.com>
2024-08-31 22:22:29 +0300

    Make Gifts And  Offers

--- Areas/Customer/Controllers/GiftController.cs
+++ Areas/Customer/Controllers/GiftController.cs
@@ -1,13 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Demo.Repository.IRepository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Demo.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class GiftController : Controller
     {
+
+        private readonly IunitOfWork _unitOfWork;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public GiftController(IunitOfWork unitOfWork, UserManager<IdentityUser> userManager)
+        {
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var products = _unitOfWork.ProductRepository.Get(e => e.Discount.HasValue, e => e.Category, equals => equals.Brand);
+            return View(products);
         }
       
 
